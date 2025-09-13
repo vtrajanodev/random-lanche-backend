2d4082a56ed44c62cf3b0c4c133b3fd84b69caf6
Victor Trajano <victor.silva@koper.com.br>
2025-09-13 11:08:37 -0300

    feat: deleting files i will not use anymore

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from "@nestjs/common";
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
+
 import { PrismaService } from "./prisma/prisma.serivce";
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService, PrismaService],
+  controllers: [],
+  providers: [PrismaService],
 })
 export class AppModule {}
